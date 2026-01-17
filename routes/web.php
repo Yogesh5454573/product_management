c4ddfd753a74bdb6953b705572afd685aa82d6d8
shrishriachugatla-defined <shrishriachugatla@gmail.com>
2026-01-17 08:19:26 +0530

    Add welcome route and view for product management

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\backend\Auth\LoginController;
 use App\Http\Controllers\backend\{ AdminController, HomeController, ProductController };
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 Route::group(['prefix' => 'admin'], function () {
