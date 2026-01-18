72f649c01f051f89d0b4bcea949c77e6218f2035
Yogesh5454573 <bachutey@gmail.com>
2026-01-18 18:47:34 +0530

    Update application name in configuration to 'ProductManagement'

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'PonumLodge'),
+    'name' => env('APP_NAME', 'ProductManagement'),
 
     /*
     |--------------------------------------------------------------------------
